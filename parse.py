ba5b82ef33af301a877c257da19313e34bf21ade
Louis Taylor <kragniz@gmail.com>
2013-02-14 22:10:57 +0000

    Tested parsing a tex command mixed with plain text

--- parse.py
+++ parse.py
@@ -1,10 +1,39 @@
 #!/usr/bin/env python
 
-import re
-
 class Parser(object):
     def __init__(self, filename):
-        self._file = open(filename)
+        self.wordChars = [c for c in 'qwertyuiopasdfghjklzxcvbnm']
+        self._tex = open(filename).read()
+        self._i = 0
+        
+    def check_current(self, *args):
+        return self.get_current() in args
+
+    def get_current(self):
+        return self._tex[self._i]
+
+    def next_current(self):
+        self._i += 1
+
+    def _read_next(self):
+        return self._file.read(1)
+
+    def normal_text(self):
+        if self.check_current(' ', *self.wordChars):
+            print self.get_current()
+            self.next_current()
+            self.normal_text()
+        elif self.check_current('\\'):
+            self.next_current()
+            self.command()
+            self.normal_text()
+
+    def command(self):
+        if self.check_current(*self.wordChars):
+            print '   ', self.get_current()
+            self.next_current()
+            self.command()
 
 if __name__ == '__main__':
-    p = Parser('exampleInput/post.tex')
+    p = Parser('exampleInput/simple.tex')
+    p.normal_text()
